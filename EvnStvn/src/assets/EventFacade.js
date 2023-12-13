35e8d9f71669f8ff7567e6d5d6cee69ff8ab4b17
MaldeFrank <maldefrankmidtgaard@gmail.com>
2023-12-13 19:24:24 +0100

    EventFacade version1 done

---
+++ EvnStvn/src/assets/EventFacade.js
@@ -0,0 +1,54 @@
+function EventFacade() {
+  let events = [{ id: 1, name: "Party", payments: [], users: [] }];
+
+  let nextId = 2;
+
+  const addEvent = (event) => {
+    event.id = nextId;
+    events.push(event);
+    nextId++;
+  };
+
+  const addUser = (user, id) => {
+    const event = events.find((event) => event.id === id);
+
+    if (event) {
+      event.users.push(user);
+    } else {
+      console.error("Event not found");
+    }
+  };
+
+  const addPayment = (payment, id) => {
+    const event = events.find((event) => event.id === id);
+
+    if (event) {
+      event.payments.push(payment);
+    } else {
+      console.error("Event not found");
+    }
+  };
+
+  const getAllEvents = () => {
+    return events;
+  };
+
+  const findEvent = (id) => {
+    events.find((event) => event.id === id);
+  };
+
+  const deleteEvent = (id) => {
+    events.filter((event) => event.id != id);
+  };
+
+  return(
+    addEvent, 
+    getAllEvents, 
+    findEvent, 
+    deleteEvent, 
+    addUser, 
+    addPayment
+  ) 
+}
+let EventFacadeFunctions = EventFacade;
+export default EventFacadeFunctions;
